1a32e0dcfa2728361e26f8aa32345b583c0c3a67
luyanchang <1114977804@qq.com>
2021-10-03 17:39:30 +0800

    replace wait with waitpid

--- c5/echo_tcp_srv.c
+++ c5/echo_tcp_srv.c
@@ -7,8 +7,11 @@ void sig_child(int signo) {
     pid_t pid;
     int stat;
 
-    pid = wait(&stat);
-    printf("child %d terminated\n", pid);
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+        printf("child %d terminated\n", pid);
+    }
+//    pid = wait(&stat);
+//    printf("child %d terminated\n", pid);
     return;
 }
 
